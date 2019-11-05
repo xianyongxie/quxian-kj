5be7d61e0c39c846d312a5428f405b99a3542f73
xiexianyong <1328067672@qq.com>
2019-11-05 22:06:54 +0800

    上传项目

---
+++ src/main/java/com/quxian/quxiankj/QuxianKjApplication.java
@@ -0,0 +1,13 @@
+package com.quxian.quxiankj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuxianKjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuxianKjApplication.class, args);
+	}
+
+}
