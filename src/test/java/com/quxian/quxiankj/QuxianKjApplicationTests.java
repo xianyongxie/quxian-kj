898622564a8d119dbcd1230cd514cad69011b082
xiexianyong <1328067672@qq.com>
2019-11-05 22:12:46 +0800

    上传项目

---
+++ src/test/java/com/quxian/quxiankj/QuxianKjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quxian.quxiankj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuxianKjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
